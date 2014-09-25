0ce05767c9b28c86a861b33fd96bbfaa8dc9ddbc
Krister Bäckman <ixevix@gmail.com>
2014-09-25 14:35:08 +0300

    Fix error messages

--- ircbot.js
+++ ircbot.js
@@ -39,7 +39,7 @@ var startListening = function(){
 	console.log('Connecting');
 	n = notifier(config.imap).on('mail', processMail);
 	n.on('end', function(){ reconnect('Disconnected'); });
-	n.on('error', function(){ reconnect(err); });
+	n.on('error', function(err){ reconnect('Error: ' + err); });
 	n.start();
 };
 
