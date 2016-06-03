5abdc68f0ced9ce0fbe71686dc33e7521406b28b
Krister Bäckman <ixevix@gmail.com>
2016-06-03 11:41:40 +0300

    bug fixes
    twitch changed their email format so offset the split, we don't need to free actualMail as it will cause problems.

--- ircbot.js
+++ ircbot.js
@@ -53,14 +53,13 @@ var parseMail = function(errors, window){
 		$ = require('jquery')(window);
 		output = $(actualMail.html).find('strong').find('a').text();
 		$.each(config.irc.channels, function(i, channel){
-			who = output.split(' ')[0];
+			who = output.split(' ')[16];
 			bot.say(channel, "Stream online: http://twitch.tv/" + who);
 		});
 		$ = null;
 		output = null;
 		who = null;
 	}
-	actualMail = null;
 	window.close();
 };
 
